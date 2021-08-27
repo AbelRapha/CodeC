4f6761937d8bf2fa9874c5d21953a423ff25ca3f
Abel <79184789+AbelRapha@users.noreply.github.com>
2021-08-26 22:26:52 -0300

    Add files via upload

---
+++ Variaveis.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+char nome[5] = "Abel";
+char sexo = 'M';
+int idade = 21;
+float altura = 1.75;
+
+//printf("Nome: %s \n Sexo: %c \n Idade: %i \n altura: %f1.2f", nome,sexo,idade,altura);
+printf("%s", nome);
+return 0;
+}
---
+++ variaveis2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+int laranjaMatheus, laranjaMarcelo, resultado;
+laranjaMarcelo = 5;
+laranjaMatheus = 39;
+resultado = laranjaMarcelo + laranjaMatheus;
+
+printf("resultado %d", resultado);
+
+return 0;
+
+}
