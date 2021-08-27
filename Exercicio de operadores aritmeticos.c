b4b3f837574245c56c8dc408f91388c61468e911
Abel <79184789+AbelRapha@users.noreply.github.com>
2021-08-26 21:22:07 -0300

    Add files via upload

---
+++ Exercicio de operadores aritmeticos.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+  printf("Eu quero ser um aluno nota 10 \n");
+  printf("Maria tem 40 anos e Joao tem 52 anos. \n A soma da idade deles e igual a: %d", 40+52);
+  printf("\n Marcelo gastou em sua compra na feira %2.2f reais ao comprar 5 tomates \n",5.0*2.3);
+  printf("Joana gastou 57.5 reais ao comprar 23 caixas de leite condensado, logo cada caixa saiu por %1.2f reais \n", 57.5/23.0);
+return 0;
+}
